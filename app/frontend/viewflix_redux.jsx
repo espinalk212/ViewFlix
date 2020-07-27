553623b715c43ed1115e6be759c03c84aa9de1c1
espinalk212 <espinalk212@gmail.com>
2020-07-27 18:51:49 -0400

    fixing things

---
+++ app/frontend/viewflix_redux.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  // const store = ConfigureStore();
+  ReactDOM.render(<h1>ViewFlix</h1>, root)
+});
+
