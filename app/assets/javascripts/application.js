601385296e36f53448d318eaed86b98c1b7fa46b
espinalk212 <espinalk212@gmail.com>
2020-07-27 22:32:32 -0400

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
+//= require rails_ujs
 //= require activestorage
 //= require_tree .
--- frontend/viewflix.jsx
+++ frontend/viewflix.jsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  debugger;
   // const store = ConfigureStore();
   ReactDOM.render(<h1>Welcome to ViewFlix</h1>, root)
 });
