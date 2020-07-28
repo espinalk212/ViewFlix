1051b91f07a6f60d80612bb25c342b66c7bb45c6
espinalk212 <espinalk212@gmail.com>
2020-07-28 18:28:34 -0400

    frontend auth

---
+++ frontend/components/loginForm/login_form_container.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import LoginForm from './login_form';
+import { login } from '../../actions/session_actions'
+
+const msp = state => ({
+  errors: state.errors.session,
+  formType: 'login'
+});
+
+const mdp = dispatch => ({
+  processForm: user => dispatch(login(user))
+});
+
+export default connect(msp, mdp)(LoginForm);
